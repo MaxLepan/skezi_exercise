69c4c9c48a43950f09b7537b3a137c1f1caa6ef2
MaxLepan <max.lepan@laposte.net>
2026-02-08 17:18:50 +0100

    Add @nestjs/config dependency and configure global settings in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { ReservationsModule } from './reservations/reservations.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, AuthModule, RoomsModule, ReservationsModule],
+  imports: [PrismaModule, AuthModule, RoomsModule, ReservationsModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
